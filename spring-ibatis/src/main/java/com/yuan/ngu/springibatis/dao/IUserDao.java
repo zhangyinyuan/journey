471552f300ef611430c164d2fe29158a820fb611
zhangyinyuan <zhangyinyuan@aliyun.com>
2019-01-10 13:29:11 +0800

    删除mybatis测试moudel

--- spring-ibatis/src/main/java/com/yuan/ngu/springibatis/dao/IUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.yuan.ngu.springibatis.dao;
-
-import com.yuan.ngu.springibatis.model.User;
-
-public interface IUserDao {
-    User selectById(Integer id);
-}
--- spring-ibatis/src/main/java/com/yuan/ngu/springibatis/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.yuan.ngu.springibatis.service;
-
-import com.yuan.ngu.springibatis.model.User;
-
-public interface IUserService {
-    User findById(Integer id);
-}
