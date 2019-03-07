ccfe0e325c57f7e538ce28300e9fdc195588dcce
zhangyinyuan <zhangyinyuan@aliyun.com>
2019-03-07 16:26:46 +0800

    删除多于的垃圾代码

--- java-test/src/main/java/Test.java
+++ java-test/src/main/java/Test.java
@@ -1,19 +1,10 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-        String createTime = "2019-02-25";
-        Calendar calendar = Calendar.getInstance();
-        try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
-            calendar.setTime(simpleDateFormat.parse(createTime));
-            calendar.add(Calendar.DATE, -1);
-            createTime = simpleDateFormat.format(calendar.getTime());
-            System.out.println(createTime);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        System.out.println(new BigDecimal(1).compareTo(null));
     }
+
 }
--- spring-boot-docker/src/main/java/com/yuan/ngu/spring/boot/docker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/yuan/ngu/spring/boot/docker/SpringBootDockerApplication.java
@@ -1,13 +1,25 @@
 package com.yuan.ngu.spring.boot.docker;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerApplication.class, args);
     }
 
+    @RequestMapping(path = "/",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Object test(){
+        JSONObject jsonpObject = new JSONObject();
+        jsonpObject.put("result","0000");
+        return jsonpObject;
+    }
 }
