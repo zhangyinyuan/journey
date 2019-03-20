daf23ed0d001bab7cd4f65e4f36e02d200bcc39d
zhangyinyuan <zhangyinyuan@aliyun.com>
2019-03-21 01:47:37 +0800

    测试跨域

--- spring-boot-aspect/src/main/java/com/yuan/ngu/springbootaspect/controller/TestsAspectController.java
+++ spring-boot-aspect/src/main/java/com/yuan/ngu/springbootaspect/controller/TestsAspectController.java
@@ -1,17 +1,23 @@
 package com.yuan.ngu.springbootaspect.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/testsAspect")
+//解决跨域.如果@CrossOrigin注解controller上,表示这个类下的所有接口都支持跨域.如果是在方法上.表示当前方法支持跨域
+@CrossOrigin
 public class TestsAspectController {
 
-    @RequestMapping(path = "/")
-    public Object testsAspect() {
-//        if ("".length() == 0){
-//            throw new RuntimeException("测试异常0000");
-//        }
-        return "testsAspect";
+    @RequestMapping(path = "/testsAspect")
+    public Object testsAspect(@RequestHeader HttpHeaders headers) {
+        //获取请求头属性
+        String token = headers.get("token").get(0);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result","0000");
+        return jsonObject;
     }
 }
