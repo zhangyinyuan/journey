2bc848b808fb975f561401414458ce4608df07c6
zhangyinyuan <zhangyinyuan@aliyun.com>
2019-06-20 09:03:16 +0800

    优化

--- spring-boot-oracle/src/main/resources/mapper/CtocOrderMapper.java
+++ spring-boot-oracle/src/main/resources/mapper/CtocOrderMapper.java
@@ -1,23 +1,17 @@
 package mapper;
 
 import com.yuan.ngu.spring.boot.oracle.pojo.po.CtocOrder;
-import com.yuan.ngu.spring.boot.oracle.pojo.po.CtocOrderExample;
 import java.math.BigDecimal;
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface CtocOrderMapper {
-
     int deleteByPrimaryKey(BigDecimal orderId);
 
     int insert(CtocOrder record);
 
-    int insertSelective(CtocOrder record);
-
-
     CtocOrder selectByPrimaryKey(BigDecimal orderId);
 
-    int updateByPrimaryKeySelective(CtocOrder record);
+    List<CtocOrder> selectAll();
 
     int updateByPrimaryKey(CtocOrder record);
 }
